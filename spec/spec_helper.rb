9709e333f046a410c0fae492e34ddb89c53239ab
Kota-A-179 <ando.kota0228@gmail.com>
2024-08-26 21:31:47 +0900

    結合テストの実装

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
-#
+
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
@@ -25,6 +25,7 @@ RSpec.configure do |config|
     #     # => "be bigger than 2 and smaller than 4"
     # ...rather than:
     #     # => "be bigger than 2"
+    
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
